143077b450a5d7b319a2db9d54cbb8ed4a66a471
luizcarvalhosilva <156116231+luizcarvalhosilva@users.noreply.github.com>
2024-04-10 19:33:13 -0300

    Criação de listas reutilizáveis

--- REACT JS/my-react-app/src/App.jsx
+++ REACT JS/my-react-app/src/App.jsx
@@ -1,7 +1,23 @@
 import List from "./List.jsx"
 
 function App() {
-  return(<List/>);
+  const fruits = [{id: 1, name:"apple", calories: 95}, 
+                  {id: 2, name:"orange", calories: 45}, 
+                  {id: 3, name:"banana", calories: 105}, 
+                  {id: 4, name:"mango", calories: 159}, 
+                  {id: 5, name:"pineapple", calories: 37}];
+
+  const vegetables = [
+                  {id: 6, name:"potatoes", calories: 110},
+                  {id: 7, name:"celety", calories: 15},
+                  {id: 8, name:"carrots", calories: 25},
+                  {id: 9, name:"corn", calories: 63},
+                  {id: 10, name:"broccoli", calories: 50}];
+
+  return(<>
+            <List items={fruits} category="Fruits"/>
+            <List items={vegetables} category="Vegetables"/>
+          </>);
 }
 
-export default App
+export default App 
--- REACT JS/my-react-app/src/List.jsx
+++ REACT JS/my-react-app/src/List.jsx
@@ -1,30 +1,16 @@
+function List(props){
+    
+    const category = props.category;
+    const itemList = props.items;
 
-function List(){
-    const fruits = [
-        {id: 1, name:"apple", calories: 95}, 
-        {id: 2, name:"orange", calories: 45}, 
-        {id: 3, name:"banana", calories: 105}, 
-        {id: 4, name:"mango", calories: 159}, 
-        {id: 5, name:"pineapple", calories: 37}
-    ];
+    const listItems = itemList.map(item => <li key={item.id}>
+                                            {item.name}: &nbsp; 
+                                            <span style={{fontWeight: "bold"}}>
+                                            {item.calories}</span></li>); 
 
-    // fruits.sort((a,b) => a.name.localeCompare(b.name));  //ALPHABETICAL
-    // fruits.sort((a,b) => b.name.localeCompare(a.name)); //REVERSE ALPHABETICAL
-    // fruits.sort((a,b) => b.calories - a.calories);     //REVERSE NUMERIC
-    // fruits.sort((a,b) => a.calories - b.calories);    //NUMERIC
-
-    /* 
-    *    O uso de 'key' permite definir chaves únicas para cada elemento e isso é importante, pois permite que o *  *    react identifique, de forma eficiente e otimizada, os itens adicionados, removidos ou modificados de um    *    array.
-    */
-
-    // const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
-    const highCalFruits = fruits.filter(fruit => fruit.calories >= 100);
-
-    const listItems = highCalFruits.map(highCalFruit => <li key={highCalFruit.id}>
-                                                              {highCalFruit.name}: &nbsp; 
-                                                              <span style={{fontWeight: "bold"}}>
-                                                              {highCalFruit.calories}</span></li>); 
-
-    return(<ul>{listItems}</ul>)
+    return(<>
+                <h3>{category}</h3>
+                <ol>{listItems}</ol>
+            </>);
 }
 export default List
