65dce8e3b5dc0b648446ba1477d9c50cf2925caf
luizcarvalhosilva <156116231+luizcarvalhosilva@users.noreply.github.com>
2024-05-06 14:47:37 -0300

    Project Movies in React
    Creation of a movies website with react

---
+++ REACT JS/app-movies/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/Header/Header.jsx';
+import Footer from './components/Footer/Footer.jsx';
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App;
