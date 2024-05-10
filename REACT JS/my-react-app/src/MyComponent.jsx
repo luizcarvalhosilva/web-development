42133cc6926d35312f3320e8e31e190560450d38
luizcarvalhosilva <156116231+luizcarvalhosilva@users.noreply.github.com>
2024-05-10 20:26:59 -0300

    IncrementAge
    criando mais um exemplo para o useState

--- REACT JS/my-react-app/src/MyComponent.jsx
+++ REACT JS/my-react-app/src/MyComponent.jsx
@@ -3,15 +3,23 @@ import React, {useState} from "react";
 function MyComponent() {
 
     const [name, setName] = useState("Guest");
+    const [age, setAge] = useState(0);
 
     const updateName = () => {
         setName("Luiz");
+    }
+
+    const incrementAge = () => {
+        setAge(age+1);
     }
 
     return(
         <div>
             <p>Name: {name}</p>        
             <button onClick={updateName}>Set Name</button>
+
+            <p>Name: {age}</p>        
+            <button onClick={incrementAge}>Increment Age</button>
         </div>
     )
 }
