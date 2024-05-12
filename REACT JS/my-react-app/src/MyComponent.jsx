a57798040cd309db1ffa41977a1d6658fe9ec4f7
luizcarvalhosilva <156116231+luizcarvalhosilva@users.noreply.github.com>
2024-05-12 15:26:01 -0300

    boolean + useState()
    Exemplo de uso de boolean com useState().

--- REACT JS/my-react-app/src/MyComponent.jsx
+++ REACT JS/my-react-app/src/MyComponent.jsx
@@ -4,6 +4,7 @@ function MyComponent() {
 
     const [name, setName] = useState("Guest");
     const [age, setAge] = useState(0);
+    const [isEmployed, setIsEmployed] = useState(false);
 
     const updateName = () => {
         setName("Luiz");
@@ -13,6 +14,10 @@ function MyComponent() {
         setAge(age+1);
     }
 
+    const toggleEmployedStatus = () => {
+        setIsEmployed(!isEmployed);
+    }
+
     return(
         <div>
             <p>Name: {name}</p>        
@@ -20,6 +25,9 @@ function MyComponent() {
 
             <p>Name: {age}</p>        
             <button onClick={incrementAge}>Increment Age</button>
+
+            <p>Name: {isEmployed ? "Yes":"No"}</p>
+            <button onClick={toggleEmployedStatus}>Toggle Status</button>
         </div>
     )
 }
