0f06a4d0b4867a4a0d5cd67b34f7155b21883c8f
luizcarvalhosilva <156116231+luizcarvalhosilva@users.noreply.github.com>
2024-05-18 18:47:12 -0300

    update Quantity real time
    Utilizando onChange para atualizar o valor de um número.

--- REACT JS/my-react-app/src/MyComponent.jsx
+++ REACT JS/my-react-app/src/MyComponent.jsx
@@ -3,15 +3,23 @@ import React, {useState} from "react";
 function MyComponent() {
 
     const [name, setName] = useState("Guest");
+    const [quantity, setQuantity] = useState(1);
 
     const handleNameChange = (e) => {
         setName(e.target.value);
+    }
+
+    function handleQuantityChange(event) {
+        setQuantity(event.target.value);
     }
 
     return(
         <div>
             <input value={name} onChange={handleNameChange}/>
             <p>Name: {name}</p>
+
+            <input value={quantity} onChange={handleQuantityChange} type="number"/>
+            <p>Quantity: {quantity}</p>
         </div>
     );
    
