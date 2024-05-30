edec7ce53198776de4f9ffab9f99d93d4b5b57a4
luizcarvalhosilva <156116231+luizcarvalhosilva@users.noreply.github.com>
2024-05-30 17:02:05 -0300

    updater function
    Uso da updater function no react com a nomenclatura de prev.

--- REACT JS/my-react-app/src/App.jsx
+++ REACT JS/my-react-app/src/App.jsx
@@ -1,7 +1,7 @@
-import ColorPicker from "./ColorPicker.jsx";
+import MyComponent from "./MyComponent.jsx";
 
 function App() {
-  return(<ColorPicker/>);
+  return(<MyComponent/>);
 }
 
 export default App 
