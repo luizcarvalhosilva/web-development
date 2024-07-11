622295e5de5f278d2aa6d4adb00317d39b0f9e07
luizcarvalhosilva <156116231+luizcarvalhosilva@users.noreply.github.com>
2024-07-11 19:13:59 -0300

    Learning to update objects
    Learning how to update an object in React.

--- REACT JS/my-react-app/src/MyComponent.jsx
+++ REACT JS/my-react-app/src/MyComponent.jsx
@@ -2,30 +2,28 @@ import React, {useState} from "react";
 
 function MyComponent() {
     
-    const [count, setCount] = useState(0);
+    const [car, setCar] = useState({
+        year: 2024, 
+        make: "Ford",
+        model: "Mustang"
+    })
 
-    function increment() {
-        setCount(c => c + 1);
-        setCount(c => c + 1);
-        setCount(c => c + 1);
-        setCount(c => c + 1);
-        setCount(c => c + 1);
-    }
-
-    function decrement() {
-        setCount(c => c - 1);
-    }
-
-    function reset() {
-        setCount(0);
+    function handleYearChange(event) {
+        setCar(c => ({...c, year: event.target.value}));
     }
     
-    return(
+    const handleMakeChange = (e) => (setCar(c => ({...c, make: e.target.value})));
+
+    const handleModelChange = (e) => {
+        setCar(c => ({...c, model: e.target.value}));
+    }
+
+    return (
         <div>
-            <p>Count: {count}</p>
-            <button onClick={decrement}>Decrement</button>
-            <button onClick={reset}>Reset</button>
-            <button onClick={increment}>Increment</button>
+            <p>Your favorite car is: {car.year} {car.make} {car.model}</p>
+            <input type="number" value={car.year} onChange={handleYearChange}/><br/>
+            <input type="text" value={car.make}  onChange={handleMakeChange}/><br/>
+            <input type="text" value={car.model} onChange={handleModelChange}/><br/>
         </div>
     );
 }
